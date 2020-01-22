d9fcc257903290245048063a698918a5e600ac9b
Josue Kuja <josuekujat@hotmail.com>
2020-01-22 20:22:13 -0300

    Lógica Virado no Jiraya 10 - Valores literais + exercícios

---
+++ src/ViradoNoJiraya/Aula10.java
@@ -0,0 +1,10 @@
+package ViradoNoJiraya;
+//Criar uma classe de calculo de porcentagem
+public class Aula10 {
+    public static void main(String[] args) {
+        float salario = 2500.00f;
+        float porcentagem = 30;
+        float calcularPorcentagem = salario * (porcentagem / 100);
+        System.out.println(calcularPorcentagem);
+    }
+}
