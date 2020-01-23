dc3ad9aa47d01662ae62545c43f1868d6ab87c11
Josue Kuja <josuekujat@hotmail.com>
2020-01-22 22:04:33 -0300

    Lógica Virado no Jiraya 16 - Controle de fluxo IF - ELSE pt 04

---
+++ src/ViradoNoJiraya/Aula16.java
@@ -0,0 +1,18 @@
+package ViradoNoJiraya;
+
+public class Aula16 {
+    public static void main(String[] args) {
+        int numero = 124;
+
+        if ((numero % 2) == 0){
+            System.out.println("Par");
+        }else {
+            System.out.println("Impar");
+        }
+    }
+}
+/*
+Quando usamos o resto ( % ) ele pega o resto da divisao.
+exemplo: 124 o resto é 0, ou seja, ele é par.
+exemplo 2: 243 o resto é 3, ou seja, ele é ímpar.
+ */
