88d0d1ce55047648ef9a1b334a33134cc7e96c3f
Josue Kuja <josuekujat@hotmail.com>
2020-01-22 21:48:14 -0300

    Lógica Virado no Jiraya 15 - Controle de fluxo if-else pt 03

---
+++ src/ViradoNoJiraya/Aula15.java
@@ -0,0 +1,17 @@
+package ViradoNoJiraya;
+
+public class Aula15 {
+    public static void main(String[] args) {
+        float salario = 4200.50f;
+        float calc;
+        String saber;
+        if (salario > 4500) {
+            calc = salario * 0.3f;
+            saber = "é 30%";
+        } else {
+            calc = salario * 0.15f;
+            saber = "é 15%";
+        }
+        System.out.println("O valor final é: " + calc + ", ou seja, " + saber);
+    }
+}
